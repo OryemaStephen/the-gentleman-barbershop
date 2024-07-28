d2b719ad8e2dcf982ba81f088316ac300d0779d7
Stephen Oryema <148610430+OryemaStephen@users.noreply.github.com>
2024-07-28 14:16:18 +0300

    Added Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,59 +1,60 @@
+import { useState } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
+
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="bg-blue-500 fixed w-full z-10 top-0 shadow">
-      <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
-        <div className="pl-4 flex items-center">
+      <div className="container mx-auto flex flex-wrap items-center justify-between py-2 px-4">
+        <div className="flex items-center">
           <a className="text-white font-bold text-2xl lg:text-4xl" href="#">
-            Logo
+            The Gentleman
           </a>
         </div>
-        <div className="block lg:hidden pr-4">
+        <div className="block lg:hidden">
           <button
-            id="nav-toggle"
-            className="flex items-center p-1 text-white hover:text-gray-300"
+            onClick={toggleMenu}
+            className="text-white focus:outline-none focus:shadow-outline"
           >
-            <svg
-              className="fill-current h-6 w-6"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <title>Menu</title>
-              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-            </svg>
+            {isOpen ? <FaTimes /> : <FaBars />}
           </button>
         </div>
         <div
-          className="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden mt-2 lg:mt-0 bg-blue-500 z-20"
-          id="nav-content"
+          className={`w-full lg:flex lg:items-center lg:w-auto ${isOpen ? 'block' : 'hidden'}`}
         >
-          <ul className="list-reset lg:flex justify-end flex-1 items-center">
-            <li className="mr-3">
+          <ul className="flex flex-col lg:flex-row lg:space-x-6 mt-2 lg:mt-0">
+            <li>
               <a
-                className="inline-block py-2 px-4 text-white no-underline hover:text-gray-300"
+                className="block lg:inline-block text-white no-underline py-2 px-4"
                 href="#"
               >
                 Home
               </a>
             </li>
-            <li className="mr-3">
+            <li>
               <a
-                className="inline-block py-2 px-4 text-white no-underline hover:text-gray-300"
+                className="block lg:inline-block text-white no-underline py-2 px-4"
                 href="#"
               >
                 About
               </a>
             </li>
-            <li className="mr-3">
+            <li>
               <a
-                className="inline-block py-2 px-4 text-white no-underline hover:text-gray-300"
+                className="block lg:inline-block text-white no-underline py-2 px-4"
                 href="#"
               >
                 Services
               </a>
             </li>
-            <li className="mr-3">
+            <li>
               <a
-                className="inline-block py-2 px-4 text-white no-underline hover:text-gray-300"
+                className="block lg:inline-block text-white no-underline py-2 px-4"
                 href="#"
               >
                 Contact
