e9a0f4b581e5b67d21f09bf123b0a58d9d6f9ce6
Stephen Oryema <148610430+OryemaStephen@users.noreply.github.com>
2024-07-29 10:07:10 +0300

    Updates

--- src/components/WorkingHours.jsx
+++ src/components/WorkingHours.jsx
@@ -3,21 +3,21 @@ import bg from "../assets/workinghours.jpg";
 
 const WorkingHours = () => {
   return (
-    <div className="relative w-full h-[90vh] md:h-[80vh]">
+    <div className="relative w-full h-[70vh] md:h-[80vh]">
       <div
-        className="w-full h-full bg-cover bg-center bg-fixed"
+        className="w-full h-full bg-fixed bg-center bg-cover"
         style={{
           backgroundImage: `url(${bg})`,
         }}
       ></div>
-      <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center text-white">
-        <div className="w-full h-full text-center flex flex-col justify-around pt-16 pb-20 ">
+      <div className="absolute top-0 left-0 flex items-center justify-center w-full h-full text-white">
+        <div className="flex flex-col justify-around w-full h-full pt-20 pb-20 text-center ">
           <h3 className="text-[#C3B299] text-3xl md:text-5xl pb-10 font-bold">
             We Are Working 24/7
           </h3>
-          <h5 className="text-xl md:text-2xl pb-5 ">Special opening hours</h5>
-          <div className="flex-grow border-t-2 w-1/4 mx-auto border-gray-300"></div>
-          <p className="text-lg md:text-xl pt-5">
+          <h5 className="pb-5 text-xl md:text-2xl ">Special opening hours</h5>
+          <div className="flex-grow w-1/4 mx-auto border-t-2 border-gray-300"></div>
+          <p className="pt-5 text-lg md:text-xl">
             Saturday 17th December: 9:30am - 8pm; <br />
             Saturday 24th December: 9:30am - 4pm; <br />
             Saturday 31th December: 9:30am - 4pm
