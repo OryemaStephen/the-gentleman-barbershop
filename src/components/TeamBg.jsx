bfb5f8818e458b0d3b661273b74de2a38a483950
Stephen Oryema <148610430+OryemaStephen@users.noreply.github.com>
2024-07-29 17:26:11 +0300

    Updates

--- src/components/TeamBg.jsx
+++ src/components/TeamBg.jsx
@@ -1,7 +1,7 @@
-import PropTypes from  'prop-types'
+import PropTypes from "prop-types";
 import image from "../assets/slider02.jpg";
 
-const TeamBg = ({title, text}) => {
+const TeamBg = ({ title, text }) => {
   return (
     <div className="relative w-full h-[80vh] md:pt-[65px] pt-[50px]">
       <div
@@ -11,9 +11,9 @@ const TeamBg = ({title, text}) => {
         }}
       ></div>
       <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center p-4 text-white">
-        <div className='w-full text-center'>
-          <h2 className='text-3xl md:text-5xl py-5 font-semibold'>{title}</h2>
-          <p className='text-lg'>{text}</p>
+        <div className="w-full text-center">
+          <h2 className="text-3xl md:text-5xl py-5 font-semibold">{title}</h2>
+          <p className="text-lg">{text}</p>
         </div>
       </div>
     </div>
@@ -21,8 +21,8 @@ const TeamBg = ({title, text}) => {
 };
 
 TeamBg.propTypes = {
-    title: PropTypes.string.isRequired,
-    text: PropTypes.string.isRequired,
-  };
+  title: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+};
 
 export default TeamBg;
